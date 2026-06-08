bbc038bfcafa4935aac341561da9ab1c73927b76
Yun Sheng <uewing@gmail.com>
2026-06-08 08:47:33 -0400

    drill 3

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,27 @@ use polars::prelude::*;
 #[allow(unused)]
 fn drill_1_print_schemea(df: &DataFrame) {
     println!("{:?}", df.schema());
+
+    // Schema:
+    // name: VendorID, field: Int32
+    // name: tpep_pickup_datetime, field: Datetime(Nanoseconds, None)
+    // name: tpep_dropoff_datetime, field: Datetime(Nanoseconds, None)
+    // name: passenger_count, field: Int64
+    // name: trip_distance, field: Float64
+    // name: RatecodeID, field: Int64
+    // name: store_and_fwd_flag, field: String
+    // name: PULocationID, field: Int32
+    // name: DOLocationID, field: Int32
+    // name: payment_type, field: Int64
+    // name: fare_amount, field: Float64
+    // name: extra, field: Float64
+    // name: mta_tax, field: Float64
+    // name: tip_amount, field: Float64
+    // name: tolls_amount, field: Float64
+    // name: improvement_surcharge, field: Float64
+    // name: total_amount, field: Float64
+    // name: congestion_surcharge, field: Float64
+    // name: Airport_fee, field: Float64
 }
 
 #[allow(unused)]
@@ -21,6 +42,18 @@ fn drill_2_mean_trip_distance(df: &DataFrame) -> Result<(), Box<dyn std::error::
     Ok(())
 }
 
+#[allow(unused)]
+fn drill_3_avg_tip_per_payment_type(df: DataFrame) -> Result<(), Box<dyn std::error::Error>> {
+    // payment_type — 1=credit card, 2=cash, 3=no charge, 4=dispute
+
+    let x = df
+        .lazy()
+        .group_by(["payment_type"])
+        .agg([col("tip_amount").mean()]);
+    println!("{}", x.collect()?);
+    Ok(())
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // read the parquet file here
     // print the schema
@@ -29,7 +62,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // println!("{:?}", pr.schema());
 
     let df = pr.finish()?;
-    let _ = drill_2_mean_trip_distance(&df);
+    // let _ = drill_1_print_schemea(&df);
+
+    let _ = drill_3_avg_tip_per_payment_type(df);
     // println!("{:?}", df.schema());
     // println!("{}", df.head(Some(5)));
 
