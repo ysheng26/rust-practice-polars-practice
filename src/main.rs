4c1248e821f2e7f5abefc7e4ccfd09fe7a51a788
Yun Sheng <uewing@gmail.com>
2026-06-08 07:57:41 -0400

    Printout first 5

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // read the parquet file here
     // print the schema
     let f = File::open("./data/yellow_tripdata_2024-01.parquet")?;
-    let mut pr = ParquetReader::new(f);
-    println!("{:?}", pr.schema());
+    let pr = ParquetReader::new(f);
+    // println!("{:?}", pr.schema());
+
+    let df = pr.finish()?;
+    println!("{}", df.head(Some(5)));
     Ok(())
 }
